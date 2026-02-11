cceca93133ed9ea595d05a111f501de6717b4a5b
Furydin <35685316+Furydins@users.noreply.github.com>
2026-02-11 18:41:45 +0000

    Don't go into an infinate loop scanning auras in combat!

--- WoWPro/WoWPro_Events.lua
+++ WoWPro/WoWPro_Events.lua
@@ -145,17 +145,18 @@ function WoWPro.AuraScan(tabula, filter)
     local BuffIndex = 1
     local BuffString = tabula[0] or ""
     local BuffName, _, BuffCount, _, _, _, _, _, _, BuffSpellId = WoWPro.UnitAura("player",BuffIndex,filter)
-    -- Aura scanning is now restricted in combat
     while BuffName do
-        if not ( _G.issecretvalue and _G.issecretvalue(BuffSpellId) ) then
-            tabula[BuffSpellId] = BuffCount or 1
-            if BuffString ~= "" then
-                BuffString = BuffString .. ","
-            end
-            BuffString = BuffString .. ("%s(%d)"):format(BuffName, BuffSpellId)
-            BuffIndex = BuffIndex + 1
-            BuffName, _, BuffCount, _, _, _, _, _, _, BuffSpellId = WoWPro.UnitAura("player",BuffIndex,filter)
+        -- Aura scanning is now restricted in combat
+        if _G.issecretvalue and _G.issecretvalue(BuffSpellId)  then
+            break
         end
+        tabula[BuffSpellId] = BuffCount or 1
+        if BuffString ~= "" then
+            BuffString = BuffString .. ","
+        end
+        BuffString = BuffString .. ("%s(%d)"):format(BuffName, BuffSpellId)
+        BuffIndex = BuffIndex + 1
+        BuffName, _, BuffCount, _, _, _, _, _, _, BuffSpellId = WoWPro.UnitAura("player",BuffIndex,filter)
     end
 
     tabula[0] = BuffString
